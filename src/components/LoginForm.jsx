2e3cbcef48ab5602cd04dbdcd51642a4dff6e120
pavan/1130 <bpavankumar516@gmail.com>
2023-09-26 20:10:41 +0530

    recet

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -32,7 +32,7 @@ const LoginFrom = () => {
   return (
     <div className="wrapper">
       <div className="form">
-        <h1 className="title">Chat Application</h1>
+        <h1 className="title">Team Chat</h1>
         <form onSubmit={handleSubmit}>
           <input
             type="text"
